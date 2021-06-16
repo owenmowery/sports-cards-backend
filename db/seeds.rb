c65d29d9fe9d849850de3a907792cbe13067a5cd
Owen Mowery <owenmowery99@gmail.com>
2021-06-16 12:40:08 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create(
+    name: "Owen Mowery",
+    email: "owenmowery99@gmail.com",
+    password: "password"
+)
+
+Card.create(
+    
+)
+create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "cards", force: :cascade do |t|
+    t.string "player_name"
+    t.string "player_team"
+    t.string "league"
+    t.string "position"
+    t.string "card_value"
+    t.string "card_img"
+    t.integer "admin_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["admin_id"], name: "index_cards_on_admin_id"
+  end
+
